0d5825866e08709cc190dd9d4e27258f9aa2aae8
awesomeo184 <awesomeo184@gmail.com>
2022-09-06 11:10:24 +0900

    cache 실습

---
+++ cache/src/main/java/com/example/cachecontrol/CacheControlInterceptor.java
@@ -0,0 +1,20 @@
+package com.example.cachecontrol;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CacheControlInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        String cacheControl = CacheControl.noCache()
+                .cachePrivate()
+                .getHeaderValue();
+        response.setHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+    }
+}
--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,5 +1,6 @@
 package com.example.cachecontrol;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,7 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+
+    @Bean
+    public CacheControlInterceptor cacheControlInterceptor() {
+        return new CacheControlInterceptor();
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry
+                .addInterceptor(cacheControlInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/resources/**");
     }
 }
